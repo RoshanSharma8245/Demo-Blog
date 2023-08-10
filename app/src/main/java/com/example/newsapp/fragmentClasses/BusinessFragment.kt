7a5a690c56c1571d9dd13f708945c8bb04fbdf09
RoshanSharma8245 <roshan@conscent.ai>
2023-08-10 17:56:16 +0530

    aar file update

--- app/src/main/java/com/example/newsapp/fragmentClasses/BusinessFragment.kt
+++ app/src/main/java/com/example/newsapp/fragmentClasses/BusinessFragment.kt
@@ -1,13 +1,23 @@
 package com.example.newsapp.fragmentClasses
 
+import ai.conscent.regularpaywalls.RegularPaywall
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.conscent.framework.core.Conscent
+import com.conscent.framework.core.ConscentWrapper
+import com.conscent.framework.core.OnConscentListener
 import com.example.newsapp.MainActivity
 import com.example.newsapp.NewsModel
 import com.example.newsapp.R
@@ -23,7 +33,33 @@ import com.example.newsapp.utils.Constants.NEWS_SOURCE
 import com.example.newsapp.utils.Constants.NEWS_TITLE
 import com.example.newsapp.utils.Constants.NEWS_URL
 
-class BusinessFragment : Fragment() {
+class BusinessFragment : Fragment(), OnConscentListener {
+
+    private lateinit var tv_status: TextView
+    private lateinit var paywall_view: FrameLayout
+    private lateinit var parent_view: ConstraintLayout
+    lateinit var conscent: Conscent
+
+    private var showSubscriptions: Boolean = false
+
+    var TAG = "BusinessFragment"
+    override fun onResume() {
+        super.onResume()
+        if (!showSubscriptions) {
+            conscent.checkContentAccess(
+                "",
+                "",
+                canSubscribe = true,
+                showClose = true
+            )
+        } else {
+            conscent.checkSubscriptions(
+                "",
+                "",
+                false,
+            )
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -63,6 +99,72 @@ class BusinessFragment : Fragment() {
         })
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        parent_view = view.findViewById<ConstraintLayout>(R.id.parent)
+        paywall_view = view.findViewById<FrameLayout>(R.id.frame)
+//        tv_status = view.findViewById<TextView>(R.id.tv_status)
+
+
+
+
+
+        conscent = ConscentWrapper.getConscentInstance(
+            requireActivity(),
+            parent_view,
+            paywall_view,
+            "1",
+            this
+        )
+        RegularPaywall.initRegularPaywall()
+
+
+        showSubscriptions = false
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        Log.i(TAG, "RedirectionHandler.onActivityResult: ")
+        if (resultCode == AppCompatActivity.RESULT_OK) {
+            if (data?.getStringExtra("TYPE") == "PLANS") {
+                conscent.checkSubscriptions(
+                    "",
+                    "",
+                )
+            } else
+                conscent.handledIntent()
+        }
+    }
+
+    /** You can pass a listener which will get called after success or failure in processing.
+     *  If you pass a listener, after successful processing, the success reference will be called and for a failed event,
+     *  the failure event will be called  **/
+    override fun onAdFree(clientId: String, contentId: String?) {
+        Log.d(TAG, "onAdFree: ")
+    }
+
+    override fun onBuyPass(clientId: String, contentId: String) {
+        Log.d(TAG, "onBuyPass: ")
+    }
+
+    override fun onError(clientId: String, contentId: String, errorMsg: String) {
+        Log.e(TAG, "onError: $errorMsg")
+    }
+
+    override fun onSignIn(clientId: String, contentId: String) {
+        Log.d(TAG, "signIn: ")
+    }
+
+    override fun onSubscribe(clientId: String, contentId: String) {
+        Log.d(TAG, "onSubscribe: ")
+    }
+
+    override fun onSuccess(clientId: String, contentId: String) {
+        Log.d(TAG, "onSuccess: ")
     }
 
 }
